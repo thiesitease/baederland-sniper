61ccf2d7408d59433a457f592223ec62d0205cc8
Thies Reinhold <reinhold@gemelo.de>
2021-12-28 12:23:18 +0100

    Erweiterung auf Impftermin

--- gemelo.baederland.sniper.app/Program.cs
+++ gemelo.baederland.sniper.app/Program.cs
@@ -16,13 +16,15 @@ namespace gemelo.baederland.sniper.app
         {
             Console.WriteLine("Baederland Sniper");
 
-            SendEmail("Baederland Sniper wurde gestartet", "http://www.gemelo.de", "[Start]");
+            SendEmail("Hamburg Impft Sniper wurde gestartet", "https://www.hamburg-impft.de/", "[Start]");
+            //SendEmail("Baederland Sniper wurde gestartet", "http://www.gemelo.de", "[Start]");
 
             while (true)
             {
-                ScanSiteForFreePlace("https://www.baederland-shop.de/schwimmschule/bronze-23323.html");
+                ScanSiteForImpfTermine("https://www.hamburg-impft.de/");
+                //ScanSiteForFreePlace("https://www.baederland-shop.de/schwimmschule/bronze-23323.html");
                 Thread.Sleep(TimeSpan.FromSeconds(5));
-                ScanSiteForFreePlace("https://www.baederland-shop.de/schwimmschule/bronze-23324.html");
+                //ScanSiteForFreePlace("https://www.baederland-shop.de/schwimmschule/bronze-23324.html");
 
                 //ScanSiteForExistingCourse("https://www.baederland.de/kurse/kursfinder/?course%5Blocation%5D=&course%5Blatlng%5D=&course%5Bpool%5D%5B%5D=15&course%5Bcategory%5D%5B%5D=85&course%5Bdate%5D=");
 
@@ -98,22 +100,70 @@ namespace gemelo.baederland.sniper.app
             }
         }
 
-        public static void SendEmail(string subject, string url, string content)
+        private static void ScanSiteForImpfTermine(string url)
+        {
+            Console.WriteLine("{0}: ScanSiteForExistingCourse bei: {1}", DateTime.Now, url);
+            try
+            {
+                using (var webClient = new WebClient())
+                {
+                    using (Stream response = webClient.OpenRead(url))
+                    {
+                        using (StreamReader reader = new StreamReader(response))
+                        {
+                            string text = reader.ReadToEnd();
+                            string searchText = "Keine Impfstelle mit freien Terminen gefunden";
+                            bool noCourse = text.Contains(searchText);
+                            if (!noCourse)
+                            {
+                                Console.WriteLine("Impfestelle gefunden, aber gibt es auch einen Termin?: {0}", url);
+                                string searchTextTerminStart = "<select name=\"pat[0][DatumUhrzeit]\" class=\"form-control select-alle-termine select-termin auswahl-termin\">";
+                                int indexOfTerminStart = text.IndexOf(searchTextTerminStart);
+                                if (indexOfTerminStart > 0)
+                                {
+                                    indexOfTerminStart += searchTextTerminStart.Length;
+                                    string searchTextTerminEnd = "</select>";
+                                    int indexOfTerminEnd = text.IndexOf(searchTextTerminEnd, indexOfTerminStart);
+
+                                    if ((indexOfTerminEnd - indexOfTerminStart) > 5)
+                                    {
+                                        string terminContent = text.SecureSubstring(indexOfTerminStart, indexOfTerminEnd - indexOfTerminStart);
+                                        SendEmail("Impftermin vorhanden!", url, terminContent, websiteContent: text);
+                                    }
+                                }
+                            }
+                        }
+                        response.Close();
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Fehler: {0}", exp.Message);
+            }
+        }
+
+
+        public static void SendEmail(string subject, string url, string content, string websiteContent = "")
         {
             //string to = "reinhold@gemelo.de";
             //string to = "thies@ampelsprinter.de";
-            string to = "baederland-sniper@tr.gemelo.de";
+
+            string to = "hamburg.impft.tr@gemelo.de";
+            //string to = "baederland-sniper@tr.gemelo.de";
             string from = "reinhold@gemelo.de";
             MailMessage message = new MailMessage(from, to);
             message.Subject = subject;
             message.Body =
-                $"Lieber Bäderland Sniper Empfänger.\n\n" +
+                $"Lieber Hamburg Impft Sniper Empfänger.\n\n" +
                 $"wir haben folgendes neues Ereignis für dich festgestellt:\n\n" +
                 $"{subject}\n\n" +
                 $"mit folgendem Inhalt: {content}\n\n" +
                 $"auf der URL: {url}\n\n\n" +
                 $"Herzliche Grüße\n\n" +
-                $"dein automatischer Bäderlandkursefinder";
+                $"dein automatischer HamburgImpftFinder\n\n" +
+                $"{websiteContent}";
+            //$"dein automatischer Bäderlandkursefinder";
 
             SmtpClient client = new SmtpClient("PETE-MAIL2019.pete.local");
             // Credentials are necessary if the server requires the client
